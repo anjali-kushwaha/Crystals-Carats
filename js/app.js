b166b72dfba7a41ec691ad8ac231143f80e6120f
Anjali Kushwaha <69588336+anjali-kushwaha@users.noreply.github.com>
2024-06-06 22:11:04 +0530

    added faqs pages

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const list = document.getElementById('list');
+const faqItems = document.querySelectorAll('.faq-item');
+
+list.addEventListener('click', () => {
+    faqItems.forEach(item => {
+        if (item.style.display === "none" || item.style.display === "") {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+});
